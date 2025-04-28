b179cc778112a383a0e8db3d41a8ac3110b39961
reboberr <janikrakhimov@gmail.com>
2025-04-29 00:31:02 +0600

    commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('active'); // Toggle the "active" class
+});
